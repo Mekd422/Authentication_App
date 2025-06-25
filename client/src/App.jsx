9930b6eb9171479d0f25853b8d682e84e4642486
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-25 14:05:11 +0300

    client side setup

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Register from './Auth/Register';
+import Login from './Auth/Login';
+import Dashboard from './pages/Dashboard';
+
+export const App = () => {
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route path="/Dashboard" element={<Dashboard/>} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+}
+
+
+export default App;
--- server/controller/AuthController.js
+++ server/controller/AuthController.js
@@ -28,7 +28,14 @@ exports.signup = async (req, res, next) => {
         res.status(201).json({
             status: 'success',
             message: 'User created successfully',
-            token});
+            token,
+            user: {
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.role
+            }
+        });
 
     } catch (error) {
         next(error);
@@ -38,5 +45,35 @@ exports.signup = async (req, res, next) => {
 
 // login a user
 exports.login = async (req, res, next) => {
-    
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return next(new CreateError('user not found', 404));
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return next(new CreateError('Invalid password', 401));
+        }
+        const token = jwt.sign({ _id: user._id}, "secretkey123", {
+            expiresIn: "90d"});
+        res.status(200).json({
+            status: 'success',
+            message: 'User logged in successfully',
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+    }, 
+            token
+        });
+
+    } catch (error) {
+        next(error);
+        
+    }
 }
